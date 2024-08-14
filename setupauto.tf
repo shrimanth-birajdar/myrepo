767a269aa8bfff3e14b36d773f61b02ce448c03a
shrimanth-birajdar <shribirajdar9@gmail.com>
2024-08-14 15:50:55 +0530

    --- Auto Git Commit ---

--- setupauto.tf
+++ setupauto.tf
@@ -1,7 +1,7 @@
 region = "us-east-1"
 ami = "ami-04a81a99f5ec58529"
 instance_type = "t2.micro"
-key_name = "hybrid"
+key_name = "myfirst"
 tags  =  {
     Name = "Development-server"
     Enviorment = "staging"
