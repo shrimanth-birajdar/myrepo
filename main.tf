070250cfe26a632e4ded21be795cdc1b9a6cd6bf
shrimanth-birajdar <shribirajdar9@gmail.com>
2024-08-14 16:17:01 +0530

    --- Auto Git Commit ---

--- main.tf
+++ main.tf
@@ -26,7 +26,9 @@ resource "aws_instance" "server_1" {
     ami = "ami-04a81a99f5ec58529"
     instance_type = "t2.micro"
     key_name = "myfirst.key"
-    tags =     {}
+    tags =     {
+        Name = "instance1"
+    }
 }
 
 
