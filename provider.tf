e03195f2d2ececb2eed3f8f036eb72873c9b7d2e
shrimanth-birajdar <shribirajdar9@gmail.com>
2024-08-14 15:45:24 +0530

    --- Auto Git Commit ---

--- provider.tf
+++ provider.tf
@@ -5,7 +5,7 @@ provider "aws" {
 # # Terraform for S3 Backend
 terraform {
     backend "s3" {
-    bucket = "terraform-shrimanthbir"
+    bucket = "terraform-shrimanthbirajdar"
     key    = "terraform.tfstate"
     region = "ap-south-1"
     dynamodb_table = "terraform_state_lock"
