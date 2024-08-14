3d4fc5fef63f99db0f307719f0934412107c0897
shrimanth-birajdar <shribirajdar9@gmail.com>
2024-08-14 15:47:35 +0530

    --- Auto Git Commit ---

---
+++ setupauto.tf
@@ -0,0 +1,9 @@
+region = "ap-south-1"
+ami = "ami-0ad21ae1d0696ad58"
+instance_type = "t2.micro"
+key_name = "hybrid"
+tags  =  {
+    Name = "Development-server"
+    Enviorment = "staging"
+    Owner = "jarvis"    
+}
