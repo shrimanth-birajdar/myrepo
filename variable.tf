9951eb62e790a75c7625025555e4b2c3fc016041
shrimanth-birajdar <shribirajdar9@gmail.com>
2024-08-14 17:29:48 +0530

    --- Auto Git Commit ---

--- variable.tf
+++ variable.tf
@@ -1,32 +1,3 @@
-variable  "us-east-1" {
-  type = string
-  description = "region calling"
-}
-
-variable  "ami-04a81a99f5ec58529" {
-  type = string
-  description  = "ami calling"
-}
-
-variable   "t2.micro" {
-  type = string
-  description = "server size calling"
-}
-
-variable  = "myfirst.key" {
-  type = string
-  description = "key calling"
-}
-
-variable  "tags" {
-  type = map
-  description = "calling the tag here"
-}
-
-variable = "value" {
-  type = string
-  description = "output block calling"
-}
 
 
 
