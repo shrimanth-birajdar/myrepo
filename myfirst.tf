cef6397749265a4dce259acf0a7ced2a33bcee18
shrimanth-birajdar <shribirajdar9@gmail.com>
2024-08-14 15:38:13 +0530

    --- Auto Git Commit ---

--- myfirst.tf
+++ myfirst.tf
@@ -1,16 +1,3 @@
-d
-# State locking using dynamodb
-resource "aws_dynamodb_table" "state_lock_table" {
-  name           = "terraform_state_lock"
-  billing_mode   = "PAY_PER_REQUEST"
-  hash_key       = "LockID"
-  attribute {
-    name = "LockID"
-    type = "S"
-  }
-}
 
-
-}
 
 
