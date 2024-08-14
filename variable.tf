f0e27e09d8ddc00d9dbc1ab63b961e501fb61c5d
shrimanth-birajdar <shribirajdar9@gmail.com>
2024-08-14 15:52:36 +0530

    --- Auto Git Commit ---

---
+++ variable.tf
@@ -0,0 +1,29 @@
+#variable block calling all values
+variable "" {
+    # default = "ap-south-1"
+    description = "region calling"
+}
+
+variable "ami" {
+    # default = "ami-0ad21ae1d0696ad58"
+    description = "calling ami"
+}
+
+variable "instance_type" {
+#   default = "t2.micro"
+  description = "calling instance type"
+}
+
+variable "key_name" {
+    # default = "hybrid"
+    description = "calling key-pair"
+}
+
+variable "tags" {
+    type = map
+    # default = {
+    # Name = "Development-server"
+    # Enviorment = "staging"
+    # Owner = "jarvis"
+    # }
+}
