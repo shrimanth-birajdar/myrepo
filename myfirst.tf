e6dc9018fd234910aa5257fd0aae78dc2aa75c1c
shrimanth-birajdar <shribirajdar9@gmail.com>
2024-08-14 15:02:25 +0530

    --- Auto Git Commit ---

---
+++ myfirst.tf
@@ -0,0 +1,32 @@
+resource "aws_s3_bucket" "terraform-state" {
+  bucket = "terraform-state-bucket-cdec-b37"
+}
+
+resource "aws_s3_bucket_versioning" "terraform-state" {
+  bucket = aws_s3_bucket.terraform-state.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+# State locking using dynamodb
+resource "aws_dynamodb_table" "state_lock_table" {
+  name           = "terraform_state_lock"
+  billing_mode   = "PAY_PER_REQUEST"
+  hash_key       = "LockID"
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
+
+
+# resource block for server creation
+resource "aws_instance" "server_1" {
+    ami = var.ami
+    instance_type = var.instance_type
+    key_name = var.key_name
+    tags = var.tags
+}
+
+
