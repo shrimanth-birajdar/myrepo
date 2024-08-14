ddf3a485e6989ce9915b19a1a1f7478e9b29747c
shrimanth-birajdar <shribirajdar9@gmail.com>
2024-08-14 15:34:22 +0530

    --- Auto Git Commit ---

--- myfirst.tf
+++ myfirst.tf
@@ -24,7 +24,7 @@ resource "aws_dynamodb_table" "state_lock_table" {
 # resource block for server creation
 resource "aws_instance" "server_1" {
     ami = 
-    instance_type = var.instance_type
+    instance_type = var.i
     key_name = var.key_name
     tags = var.tags
 }
