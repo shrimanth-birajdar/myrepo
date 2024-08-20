dbc77719d97368e3d01b2babfc4802853f1e958b
shrimanth-birajdar <163817644+shrimanth-birajdar@users.noreply.github.com>
2024-08-20 18:53:37 +0530

    Update mysecond.tf

--- mysecond.tf
+++ mysecond.tf
@@ -1,1 +1,41 @@
 
+provider "aws" {
+    region = var.region
+
+}
+resource "aws_instance" "myinstance" {
+  ami = var.ami
+  instance_type = var.instance_type
+  key_name = var.key_name
+  tags = var.tags
+  vpc_security_group_ids = var.vpc_security_group_ids
+}
+
+variable "region" {
+    description = "plz enter aws region"
+    default = "us-east-1"
+}
+variable "key_name" {
+ default =  "terrfromkey"
+}
+variable "tags" {
+    type = map
+    default =  {
+     env = "dev"
+    Name = "instance1" }
+
+
+}
+variable "vpc_security_group_ids" {
+        type = list
+        default = ["sg-095d16c928a4862ed"]
+
+}
+
+variable "ami" {
+        default = "ami-04a81a99f5ec58529"
+}
+
+variable "instance_type" {
+        default = "t2.micro"
+}
