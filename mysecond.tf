c0dc71ff200c4e34fad68d3af12f6a9204dc73c3
shrimanth-birajdar <shribirajdar9@gmail.com>
2024-08-21 15:21:36 +0530

    --- Auto Git Commit ---

--- mysecond.tf
+++ mysecond.tf
@@ -1,10 +1,10 @@
-terraform {
-  backend "s3" {
-    bucket = "shrimanthbirajdar-b60"
-    key    = "terraform.tfstate"
-    region = "us-east-1"
-  }
-}
+# terraform {
+#   backend "s3" {
+#     bucket = "shrimanthbirajdar-b60"
+#     key    = "terraform.tfstate"
+#     region = "us-east-1"
+#   }
+# }
 
 provider "aws" {
   region = var.region
