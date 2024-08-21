24ee43afd9fcbadf95232cb8fc5237276bce83e1
shrimanth-birajdar <shribirajdar9@gmail.com>
2024-08-21 14:50:36 +0530

    --- Auto Git Commit ---

--- mysecond.tf
+++ mysecond.tf
@@ -12,8 +12,12 @@ terraform {
     Name        = "My bucket"
     Environment = "Dev"
   }
-  
-}
+  resource "aws_s3_bucket_versioning" "versioning_example" {
+  bucket = aws_s3_bucket.example.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+
 provider "aws" {
     region = var.region
 
