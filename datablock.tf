8bcc2a17d154bc1349fc3493c5bda9a810d0062f
shrimanth-birajdar <shribirajdar9@gmail.com>
2024-08-22 15:27:03 +0530

    --- Auto Git Commit ---

--- datablock.tf
+++ datablock.tf
@@ -24,7 +24,7 @@ resource "aws_instance" "myinstance" {
   instance_type          = var.instance_type
   key_name               = var.key_name
   tags                   = var.tags
-  vpc_security_group_ids = []
+  vpc_security_group_ids = [""]
 }
 
 variable "region" {
